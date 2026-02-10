f335ecbdcc451be2a7a6d6d2e7e783bf050dd26f
Bfr-JHon <jhonathavinicius21@gmail.com>
2026-02-10 10:09:22 -0300

    Iniciando Menu loja de Jogos

---
+++ Menu.ts
@@ -0,0 +1,98 @@
+
+import { Input } from "./src/util/Input";
+
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("               Steam Melhorada                           ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Produto                         ");
+        console.log("            2 - Listar todas os Produtos               ");
+        console.log("            3 - Buscar Produto por Id                  ");
+        console.log("            4 - Atualizar Dados do Produto             ");
+        console.log("            5 - Apagar Produto                        ");
+       // console.log("          6 - Buscar por Nome  do produto             ");
+        console.log("            0 - sair                                   ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+
+        console.log("Entre com a opção desejada: ");
+        opcao = Input.questionInt("");
+
+        if (opcao == 0) {
+            console.log("\nSteam Melhorada - Nao vendemos so jogos, vendemos Diversão!");
+            sobre();
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log("\n\nCriar Conta\n\n");
+                keyPress();
+
+                break;
+            
+                case 2:
+                console.log("\n\nListar todas os Produtos\n\n");
+                    keyPress();
+                break;
+            
+                case 3:
+                console.log("\n\nBuscar Por Id\n\n");
+                        keyPress();
+                break;
+          
+                case 4:
+                console.log("\n\nAtualizar dados dos produtos\n\n");
+                        keyPress();
+                break;
+          
+                case 5:
+                console.log("\n\nApagar Produto\n\n");
+                    keyPress();
+                break;
+            
+                // case 6 :
+                // console.log("\n\nProcurar por Nome\n\n");
+                //     keyPress();
+                // break;
+            
+
+              default:
+
+                console.log("\nOpção Inválida!\n");
+                        keyPress();
+                break;
+        }
+    }
+
+}
+
+/* Função com os dados da pessoa desenvolvedora */
+
+export function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("\n          Steam melhorada                               ");
+    console.log("Projeto Desenvolvido por: Jhonatha Vinicius");
+    console.log("Jhonatha - generation@generation.org");
+    console.log("github.com/bfr-jhon");
+    console.log("*****************************************************");
+}
+
+
+// Funcao KeyPress
+function keyPress(): void{
+    console.log("Pressione Enter para Continuar...");
+    Input.prompt();
+}
+
+main();
